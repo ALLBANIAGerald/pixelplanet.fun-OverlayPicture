9a1c061b05acde0ddcb19ce41f58629ca2b0ea7f
Woyken <13166802+Woyken@users.noreply.github.com>
2024-07-15 18:25:03 +0300

    update nested signal to handle default returns

--- src/utils/signalPrimitives/createSignalComputedNested.ts
+++ src/utils/signalPrimitives/createSignalComputedNested.ts
@@ -3,7 +3,11 @@ import { Signal } from 'signal-polyfill';
 interface AnySignal<T> {
     get: () => T;
 }
-export function createSignalComputedNested<T>(computation: () => AnySignal<T>) {
-    const nested = new Signal.Computed(() => computation());
-    return new Signal.Computed(() => nested.get().get());
+export function createSignalComputedNested<T>(computation: () => AnySignal<T> | T) {
+    const wrapper = new Signal.Computed(() => computation());
+    return new Signal.Computed(() => {
+        const nested = wrapper.get();
+        if (nested && typeof nested === 'object' && 'get' in nested) return nested.get();
+        return nested;
+    });
 }
