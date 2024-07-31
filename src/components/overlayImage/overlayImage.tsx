0beb422187207887428cee49df40b9b87c110abb
Woyken <13166802+Woyken@users.noreply.github.com>
2024-07-31 15:28:08 +0300

    Add button placeholder that will upload new file

--- src/components/overlayImage/overlayImage.tsx
+++ src/components/overlayImage/overlayImage.tsx
@@ -216,6 +216,13 @@ function OverlayImageWithControls(props: { template: { imageId: number; x: numbe
                     '--height': `${sizeOnScreen().height.toString()}px`,
                 }}
             >
+                <div class="tw-pointer-events-auto tw-flex -tw-translate-y-full">
+                    <button class="tw-btn tw-btn-primary tw-h-12 tw-w-12 tw-p-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="tw-h-6 tw-w-6" viewBox="0 -960 960 960" fill="currentColor">
+                            <path d="M720-330q0 104-73 177T470-80q-104 0-177-73t-73-177v-370q0-75 52.5-127.5T400-880q75 0 127.5 52.5T580-700v350q0 46-32 78t-78 32q-46 0-78-32t-32-78v-370h80v370q0 13 8.5 21.5T470-320q13 0 21.5-8.5T500-350v-350q-1-42-29.5-71T400-800q-42 0-71 29t-29 71v370q-1 71 49 120.5T470-160q70 0 119-49.5T640-330v-390h80v390Z" />
+                        </svg>
+                    </button>
+                </div>
                 <div
                     class="tw-absolute tw-left-[--left-offset] tw-top-[--top-offset] -tw-translate-x-1/2 -tw-translate-y-1/2 tw-transition-[left,top] [transition-timing-function:linear(0.2_0%,1_100%)]"
                     classList={{
